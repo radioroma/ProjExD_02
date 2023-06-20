13deb3dcac58602e67cb1b1561b0195a5ab0b8ac
Sunager <c0b220829c@deu.teu.ac.jp>
2023-06-20 17:07:10 +0900

    演習1　途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+#演習1途中
 import random
 import sys
 import pygame as pg
@@ -19,7 +20,21 @@ delta = {
     pg.K_LEFT:(-5,0),
     pg.K_RIGHT:(5,0),
     }
-
+def asdf():
+    kk_img0 =pg.transform.rotozoom(pg.image.load("ex02/fig/3.png"),0, 2.0)
+    kk_img = pg.transform.flip(kk_img0, True, False)
+    return {
+        (0,0):kk_img,#キーを押してない時
+        (+5, 0):kk_img,  # 右
+        (+5, -5):pg.transform.rotozoom(kk_img, 45, 1.0),  # 右上
+        (0, -5):pg.transform.rotozoom(kk_img, 90, 1.0), # 上            
+        (-5, -5):pg.transform.rotozoom(kk_img, -45, 1.0),# 左上
+        (-5, 0):kk_img,
+        (-5, +5):pg.transform.rotozoom(kk_img, -45, 1.0),              
+        (0, +5):pg.transform.rotozoom(kk_img, -90, 1.0),  
+        (+5, +5):pg.transform.rotozoom(kk_img, 45, 1.0),
+        }
+   
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -27,7 +42,8 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
-    kk_rct=kk_img.get_rect()
+    kk_imgs=asdf()
+    kk_rct=kk_imgs[(0,0)].get_rect()
     kk_rct.center=900,400
     bd_img= pg.Surface((20,20))
     pg.draw.circle(bd_img,(255,0,0), (10,10 ), 10)
@@ -38,6 +54,7 @@ def main():
     #爆弾surface
     bd_rct.center = x,y 
     vx,vy = +5,+5
+   
     
     clock = pg.time.Clock()
     tmr = 0
@@ -50,17 +67,24 @@ def main():
             return 
         
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]  # 合計移動量
+        # kk_img =asdf[(sum_mv)]
+        sum_mv = [0, 0] 
+        sum_kk_mv=[0,0] 
         for k, mv in delta.items():
             if key_lst[k]: 
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-        kk_rct.move_ip(sum_mv)
+                sum_kk_mv[0] +=mv[0]
+                sum_kk_mv[1] +=mv[1]
+        #screen.blit(kk_img,kk_rct)
+                kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) !=(True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+
+        
             
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_imgs[tuple(sum_mv)], kk_rct)
         bd_rct.move_ip(vx, vy)
         yoko,tate = check_bound(bd_rct)
         if not yoko:
